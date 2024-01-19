ad5ceaf6fc9ce0e3dc717fe18c1b22defa664c31
S3Gold <157092659+S3Gold@users.noreply.github.com>
2024-01-19 07:16:56 +0100

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['917605902011', 'GURU', true],
-  ['6281255369012', 'Ash', true], 
+  ['8143510159', 'S3Gold', true],
+  ['9154862501', 'S3Media', true], 
   [''] 
 ] //Number of owners
 
